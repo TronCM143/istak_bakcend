8f5eb81213188a2f5d5033726f07825a18520018
froilan <e.froilandave@gmail.com>
2025-10-21 22:20:42 +0800

    hello

--- istak_backend/firebase.py
+++ istak_backend/firebase.py
@@ -6,7 +6,7 @@ from firebase_admin import credentials, messaging
 # Get the current directory of this file
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-cred_path = os.path.join(BASE_DIR, "serviceAccountKey.json")
+cred_path = os.getenv("FIREBASE_CREDENTIALS")
 
 if not firebase_admin._apps:
     cred = credentials.Certificate(cred_path)
